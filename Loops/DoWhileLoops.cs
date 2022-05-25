4bed677f9072405ee124435002c637fb8fbca746
nik <nikblvck@icloud.com>
2022-05-25 09:23:40 -0700

    do while loops complete

---
+++ Loops/DoWhileLoops.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace DoWhileLoop
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      bool buttonClick = true;
+
+      do
+      {
+        Console.WriteLine("Bing bong!! BING BONG! Wakey wakey!");
+      } while (!buttonClick);
+    Console.WriteLine("Time for a five minute break");
+    }
+  }
+}
